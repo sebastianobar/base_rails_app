c23001a542d6cc7d60a97fc140e49236e1e9c90a
Sebastiano <sebastiano.barravecchia@gmail.com>
2025-10-22 08:41:49 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_22_074143) do
+ActiveRecord::Schema[8.1].define(version: 2025_08_30_142703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
