b054488e8fc0e4eb3f884e74fe2a129d5a033c20
Sebastiano <sebastiano.barravecchia@gmail.com>
2025-11-22 14:01:35 +0000

    ensure routes are loaded for jobs handler to support Devise functionality

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Ensure routes are loaded even on jobs handler (needed for Devise in some cases)
+Rails.application.reload_routes_unless_loaded
 ActiveJob::Base.include RailsPerformanceActiveJob
